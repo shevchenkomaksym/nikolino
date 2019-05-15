41f6bd86f698b4c4698e450ec8da81903c58f162
Archangel2903 <archangel2903@gmail.com>
2019-05-15 13:35:23 +0300

    add intersectionObserver

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import 'intersection-observer';
 import $ from 'jquery';
 import 'bootstrap';
 import 'popper.js';
@@ -14,5 +15,21 @@ $(window).on('load', function () {
 });
 
 $(function () {
+    const imagesAll = document.querySelectorAll('img[data-src]');
+    let imgObserve = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio >= 0 && entry.target.hasAttribute('data-src')) {
+                let current = entry.target;
+                let source = current.getAttribute('data-src');
 
+                current.setAttribute('src', source);
+                current.removeAttribute('data-src');
+            }
+        });
+    });
+    if (imagesAll.length > 0) {
+        imagesAll.forEach(function (image) {
+            imgObserve.observe(image);
+        });
+    }
 });
